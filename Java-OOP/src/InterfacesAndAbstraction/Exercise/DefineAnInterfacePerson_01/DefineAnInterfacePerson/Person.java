284fc7a1d98c479828d3b31b1964e72f3f8af24d
FeroMah <ferhatmahmud@gmail.com>
2020-06-16 22:08:12 +0300

    Initial commit.

---
+++ Java-OOP/src/InterfacesAndAbstraction/Exercise/DefineAnInterfacePerson_01/DefineAnInterfacePerson/Person.java
@@ -0,0 +1,8 @@
+package InterfacesAndAbstraction.Exercise.DefineAnInterfacePerson_01.DefineAnInterfacePerson;
+
+public interface Person {
+
+    String getName();
+    int getAge();
+
+}
---
+++ Java-OOP/src/InterfacesAndAbstraction/Exercise/DefineAnInterfacePerson_01/Main.java
@@ -0,0 +1,31 @@
+package InterfacesAndAbstraction.Exercise.DefineAnInterfacePerson_01;
+
+
+
+import InterfacesAndAbstraction.Exercise.DefineAnInterfacePerson_01.DefineAnInterfacePerson.Citizen;
+import InterfacesAndAbstraction.Exercise.DefineAnInterfacePerson_01.DefineAnInterfacePerson.Person;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Class[] citizenInterfaces = Citizen.class.getInterfaces();
+        if (Arrays.asList(citizenInterfaces).contains(Person.class)) {
+            Method[] fields = Person.class.getDeclaredMethods();
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            int age = Integer.parseInt(scanner.nextLine());
+            Person person = new Citizen(name, age);
+            System.out.println(fields.length);
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+        }
+    }
+}
